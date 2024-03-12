f43fd84e2c4f215632d55dff3b1e7983f003a988
River Satya <river.satya@gmail.com>
2024-03-12 15:58:34 +0800

    Cleanup formatting

--- browser-extension-chrome/assets/utility.js
+++ browser-extension-chrome/assets/utility.js
@@ -1,28 +1,27 @@
 /**
-Common code to be used in the project
-**/
+ Common code to be used in the project
+ **/
 
-//ToDo:We can use better design pattern(s.a facade) here for extending this work to multiple message systems
 const _utility = {
-  getWhatsappMessageUrl: function(phonestring, cb){
-	//Returns message url in the form of "https://api.whatsapp.com/send?phone=91232132&text=hi"
- 	const MESSAGE_API_URL="https://api.whatsapp.com/send?phone="
-	chrome.storage.sync.get({'defaultCountryCode': 91, 'defaultMessage': ''}, function(configs){
-	  	if(!phonestring){
-			return cb("#NotAPhoneNumber");
-	 	}
-	 	//Format the selection - remove the non-digit characters
-	 	var phone = phonestring.substring(4).replace(/[\s-()]+/g,'');
-	 	//Create the url with the given text and message api url 
-	 	if (!phone){
-			return cb("#NotAPhoneNumber");
-	 	} else if(phone.substring(0, 1) !== '+') {
-			prefix = configs.defaultCountryCode;
-	  	} else {
-			prefix = "";
-		}
-		phone = phone.replace(/^[+0]/,''); // strip off leading characters + or 0
-	        return cb(MESSAGE_API_URL+prefix+phone+"&text="+encodeURIComponent(configs.defaultMessage));
-    });
-  }
+    getWhatsappMessageUrl: function (phonestring, cb) {
+        // Returns message url in the form of "https://api.whatsapp.com/send?phone=91232132&text=hi"
+        const MESSAGE_API_URL = "https://api.whatsapp.com/send?phone="
+        chrome.storage.sync.get({'defaultCountryCode': 1, 'defaultMessage': ''}, function (configs) {
+            if (!phonestring) {
+                return cb("#NotAPhoneNumber");
+            }
+            // Format the selection - remove the non-digit characters
+            var phone = phonestring.substring(4).replace(/[\s-()]+/g, '');
+            // Create the url with the given text and message api url
+            if (!phone) {
+                return cb("#NotAPhoneNumber");
+            } else if (phone.substring(0, 1) !== "+") {
+                prefix = configs.defaultCountryCode;
+            } else {
+                prefix = "";
+            }
+            phone = phone.replace(/^[+0]/, ''); // strip off leading characters + or 0
+            return cb(MESSAGE_API_URL + prefix + phone + "&text=" + encodeURIComponent(configs.defaultMessage));
+        });
+    }
 }
--- browser-extension-chrome/whatsappLinkInContextMenu.js
+++ browser-extension-chrome/whatsappLinkInContextMenu.js
@@ -1,18 +1,18 @@
 /**
-Code to be executed for context menu functionality
-**/
+ Code to be executed for context menu functionality
+ **/
 
 //Make sure assets/utility.js is loaded so we have access to _utility.getWhatsappMessageUrl 
-function getword(info,tab) {
-  _utility.getWhatsappMessageUrl(info.selectionText, function(whatsappMessageUrl){
-  	chrome.tabs.create({  
-    		url: whatsappMessageUrl
-	});
-  })
+function getword(info, tab) {
+    _utility.getWhatsappMessageUrl(info.selectionText, function (whatsappMessageUrl) {
+        chrome.tabs.create({
+            url: whatsappMessageUrl
+        });
+    })
 }
 
 chrome.contextMenus.create({
-  title: "Whatsapp: %s", 
-  contexts:["selection"], 
-  onclick: getword
+    title: "Whatsapp: %s",
+    contexts: ["selection"],
+    onclick: getword
 });
